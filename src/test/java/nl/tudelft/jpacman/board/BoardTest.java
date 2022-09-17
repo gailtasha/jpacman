b2e60fa12c897e2721e7d9eb926bbb098b2d8fde
Tasha Settewong <tasha.set@student.mahidol.ac.th>
2022-09-17 23:54:40 +0700

    Implement BoardTest test cases

---
+++ src/test/java/nl/tudelft/jpacman/board/BoardTest.java
@@ -0,0 +1,21 @@
+package nl.tudelft.jpacman.board;
+import static org.assertj.core.api.Assertions.assertThat;
+import org.junit.jupiter.api.Test;
+
+public class BoardTest {
+    @Test
+    void testInvariant(){
+        Square row = new BasicSquare();
+        Square[][] grid = new Square[1][1];
+        grid[0][0] = row;
+        Board board = new Board(grid);
+        assertThat(board.invariant()).isTrue();
+    }
+    @Test
+    void testSquareAt(){
+        Square[][] grid = new Square[1][1];
+        grid[0][0] = null;
+        Board board = new Board(grid);
+        assertThat(board.squareAt(0,0)).isEqualTo(null);
+    }
+}
