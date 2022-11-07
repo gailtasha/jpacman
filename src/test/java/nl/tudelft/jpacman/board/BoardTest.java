264f0bee2d649f91aec86f53db63855200c6d3dc
gailtasha <51601786+gailtasha@users.noreply.github.com>
2022-11-07 11:29:41 +0700

    Update BoardTest.java

--- src/test/java/nl/tudelft/jpacman/board/BoardTest.java
+++ src/test/java/nl/tudelft/jpacman/board/BoardTest.java
@@ -8,6 +8,9 @@ import org.junit.jupiter.api.Test;
  * @author Tasha Settewong 
  */
 public class BoardTest {
+     /**
+     * Do the invarant method perform correctly?.
+     */
     @Test
     void testInvariant() {
         Square row = new BasicSquare();
@@ -16,12 +19,16 @@ public class BoardTest {
         Board board = new Board(grid);
         assertThat(board.invariant()).isTrue();
     }
+    
+     /**
+     * Do the squareAt method perform correctly?.
+     */
     @Test
     void testSquareAt() {
         Square row = new BasicSquare();
         Square[][] grid = new Square[1][1];
         grid[0][0] = row;
         Board board = new Board(grid);
-        assertThat(board.squareAt(0,0)).isEqualTo(grid[0][0]);
+        assertThat(board.squareAt(0, 0)).isEqualTo(grid[0][0]);
     }
 }
